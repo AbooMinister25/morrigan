f3477696007fd7cab3481f881a802e0ce6fc0731
AbooMinister25 <aboominister@gmail.com>
2025-04-18 11:59:44 -0400

    Clean up recipe and move some scripts over to justfiles

--- files/scripts/installhyprlandplugins.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-hyprpm add https://github.com/Duckonaut/split-monitor-workspaces
-hyprpm enable split-monitor-workspaces
-hyprpm reload
