95437261fdfa79325c2ab9a6751cc4130585b280
AbooMinister25 <aboominister@gmail.com>
2025-04-25 20:14:46 -0400

    fixes to scripts and nwg-dock-hyprland share directory copying. Everything should work now (hopefully)

--- files/scripts/installdock.sh
+++ files/scripts/installdock.sh
@@ -12,24 +12,6 @@ dnf install -y golang
 git clone https://github.com/nwg-piotr/nwg-dock-hyprland
 cd nwg-dock-hyprland
 
-# apt-get install 
-
-# apt-get install -y libgtk-layer-shell-dev
-# apt-get install -y libgirepository1.0-dev
-# apt-get install -y libc6-dev
-# apt-get install -y libcairo2-dev
-# apt-get install -y libpango1.0-dev
-# apt-get install -y libglib2.0-dev
-# apt-get install -y libgtk-3-dev
-# apt-get install -y libglib2.0
-
-
-# apt-get install -y libgtk-layer-shell-dev
-# apt-get install -y libgirepository1.0-dev
-
-
-# dnf install -y atk cairo cairo-gobject fontconfig freetype gdk-pixbuf2 glib2 glibc gtk-layer-shell gtk3 harfbuzz hyprland pango zlib-ng-compat gobje
-
 dnf install -y gobject-introspection-devel
 dnf install -y gtk-layer-shell
 dnf install -y gtk-layer-shell-devel
